48f469e5d6bf380c30a3d62fd8d2b45762432225
fangbao <fangbao@wanglibank.com>
2017-09-21 11:41:52 +0800

    添加date方法

--- Date.js
+++ Date.js
@@ -1,15 +1,31 @@
-// 获取当前月开始时间
-function getNowMonthStartDate (date) {
-  var date = new Date();
+// 获取当前月开始时间  method1
+function getStartDateOfMonth (date) {
+  date = date || new Date();
+  date = new Date(date);
   return date.toLocaleString().match(/\d{0,4}\/\d{1,2}\/(\d{1,2})/)[0].replace(/(\d{0,4}\/\d{1,2}\/)\d{1,2}/, "$11")
 }
+// 获取当前月开始时间  method2
+function getStartDateOfMonth (date) {
+  date = new Date(date);
+  date.setDate(1);
+  return date;
+}
 
-// 获取当前月最有一天
-function getNowMonthLastDate (date) {
-  var date = new Date();
+// 获取当前月最有一天 method1
+function getLastDateOfMonth (date) {
+  date = date || new Date();
+  date = new Date(date);
   // 下一月开始
   var nextMonthStartDate = date.toLocaleString().match(/\d{0,4}\/\d{1,2}\/(\d{1,2})/)[0].replace(/(\d{0,4})\/(\d{1,2})\/(\d{1,2})/g, function(){
   	return arguments[1] + "/" + (parseInt(arguments[2]) + 1) + "/1"
   })
   return new Date(new Date(nextMonthStartDate).getTime() -1).toLocaleString().match(/\d{0,4}\/\d{1,2}\/(\d{1,2})/)[0];
+}
+// 获取当前月最有一天 method2
+function getLastDateOfMonth (date) {
+  date = date || new Date();
+  date = new Date(date);
+  date.setMonth(date.getMonth() + 1);
+  date.setDate(0);
+  return date;
 }
